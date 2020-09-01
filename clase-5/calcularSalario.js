d6eff71cdd564e47ba5ef614a8e2dc9815d2c4be
ramaffei <ramaffei@users.noreply.github.com>
2020-09-01 15:29:26 -0300

    cambio de nombre variables

--- clase-5/calcularSalario.js
+++ clase-5/calcularSalario.js
@@ -2,12 +2,11 @@
 //cuando el usuario haga click en el botón "calcular", mostrar el salario mensual
 // en una caja de texto deshabilitada. --> <input type="text" disabled id="salario-mensual"/>
 
-const $botonCalcular = document.querySelector('#botonCalcular')
+const $botonCalcular = document.querySelector('#boton-Calcular')
 $botonCalcular.onclick = function(){
-  const $salarioAnual = document.querySelector('#salarioAnual');
+  const $salarioAnual = document.querySelector('#salario-Anual');
   const resultado = $salarioAnual.value / 12;
-  let $salarioMensual = document.querySelector('#salarioMensual');
-  $salarioMensual.value = String(`Tu salario Mensual es ${resultado}`); 
-  $salarioAnual.value = "";
+  let $salarioMensual = document.querySelector('#salario-Mensual');
+  $salarioMensual.value = resultado; 
 
 }
